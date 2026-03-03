96021da4978129feb5bc737ecd0770ed97ea9a16
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2026-03-03 13:22:10 +0000

    feat(node): add provider_ref_id to payment intent response types

--- src/client.ts
+++ src/client.ts
@@ -39,6 +39,7 @@ interface PaymentIntentResponse {
   id: string;
   connection_id: string;
   provider: string;
+  provider_ref_id?: string;
   status: string;
   client_secret: string;
   psp_public_key?: string;
